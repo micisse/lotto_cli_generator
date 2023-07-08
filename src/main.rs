398355d2134a8b2d59199b873634ac782cc0bf55
MorelC <morelcisse19@gmail.com>
2023-07-08 22:55:16 +0200

    Get clap args

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,47 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author = "Morel Cissé", version = "1.0.0", about = None, long_about = None)]
 struct Args {
-    #[arg(
-        short,
-        long,
-        // value_name = "www | common | ansible | zsh | kdbx | my | browser | vscode",
-        required = true
-    )]
-    numbers: String,
+    #[arg(long, required = true)]
+    input_1: String, // numéros normales
+    #[arg(long, required = true)]
+    input_2: String, // numéros chances
+
+    #[arg(long, default_value_t = 10, required = false)]
+    grid_count: i8,
 }
 
 fn main() {
-    let default_numbers = [1..49];
-    let args = Args::parse();
-    let args_numbers: String = args.numbers;
-    let numbers = args_numbers.split(" ").collect::<Vec<&str>>();
+    // let lotto_price = 2.20; // €
+    // Chaque grille contient 49 nombres
+    let grid_numbers: Vec<i32> = (1..=49).collect();
+    // Chaque grille (côté numéro chance) contient 10 nombres
+    // let grid_chance_numbers: Vec<i32> = (1..=10).collect();
 
-    println!("{:?}", numbers);
-    println!("{:?}", default_numbers);
+    let args = Args::parse();
+    // Le nombre de grille à générer
+    let grid_count: i8 = args.grid_count;
+
+    let input_1_args: String = args.input_1;
+    let input_1 = input_1_args
+        .split(" ")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    // let input_2_args: String = args.input_2;
+    // let input_2 = input_2_args
+    //     .split(" ")
+    //     .collect::<Vec<&str>>()
+    //     .iter()
+    //     .map(|x| x.parse::<i32>().unwrap())
+    //     .collect::<Vec<i32>>();
+
+    if input_1.len() < 5 || input_1.len() > 5 {
+        panic!("'input_1' doit obligatoirement contenir {} nombres", 5);
+    }
+
+    println!("{:?}", input_1);
+    println!("{:?}", grid_numbers);
+    println!("{}", grid_count);
 }
