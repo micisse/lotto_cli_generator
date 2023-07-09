07039d3b36bfd7e4790405c2ef485caff5d0184c
MorelC <morelcisse19@gmail.com>
2023-07-09 13:49:14 +0200

    Some reviews in main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,36 +29,42 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let grid_count = args.grid_count; // Numbers of grid to generate
-    let mix: bool = args.mix; // Mix the result
+    let arg_grid_count = args.grid_count; // Numbers of grid to generate
+    let arg_mix: bool = args.mix; // Mix the result
+    let arg_numbers = args.numbers;
+    let arg_odds_numbers = args.odds_numbers;
     let mut combinaisons: Vec<Vec<i32>> = Vec::new();
     let mut odds_combinaisons: Vec<Vec<i32>> = Vec::new();
     let grid_numbers: Vec<i32> = (1..=49).collect(); // Each grid have 49 numbers
     let grid_odds_numbers: Vec<i32> = (1..=10).collect(); // Each grid (odds numbers) have 10 numbers
 
-    let mut numbers = match args.numbers {
+    let mut numbers = match arg_numbers.to_owned() {
         Some(value) => collect_input(value),
-        None => grid_numbers.clone(),
+        None => grid_numbers.to_owned(),
     };
-    let mut odds_numbers = match args.odds_numbers {
+    let mut odds_numbers = match arg_odds_numbers.to_owned() {
         Some(value) => collect_input(value),
-        None => grid_odds_numbers.clone(),
+        None => grid_odds_numbers.to_owned(),
     };
 
-    'loop_label: for idx in 1..=grid_count {
-        let mut row = 1;
+    if arg_mix && arg_numbers.is_none() {
+        panic!("numbers argument is required");
+    }
+
+    'loop_label: for idx in 1..=arg_grid_count {
         let i = idx - 1;
+        let mut category = 1;
 
-        if mix && i >= (grid_count / 2) {
+        if arg_mix && i >= (arg_grid_count / 2) {
             numbers = grid_numbers.clone();
             odds_numbers = grid_odds_numbers.clone();
-            row += 1;
+            category += 1;
         };
 
-        let new_numbers = generate_new_numbers(numbers.clone(), 5);
-        let new_odds_numbers = generate_new_numbers(odds_numbers.clone(), 1);
+        let new_numbers = generate_new_numbers(&numbers, 5);
+        let new_odds_numbers = generate_new_numbers(&odds_numbers, 1);
         let odd_number = new_odds_numbers[0];
-        let index = odds_numbers.iter().position(|&r| r == odd_number).unwrap();
+        let odd_index = odds_numbers.iter().position(|&r| r == odd_number).unwrap();
 
         if combinaisons.contains(&new_numbers) {
             println!("\n----| SKiP\n");
@@ -67,10 +73,14 @@ fn main() {
             break 'loop_label;
         }
 
-        combinaisons.push(new_numbers.clone());
-        odds_numbers.remove(index);
-        odds_combinaisons.push(new_odds_numbers.clone());
-        println!("----| Row {} | Combination n°{}", row, idx);
+        combinaisons.push(new_numbers.to_owned());
+        odds_combinaisons.push(new_odds_numbers.to_owned());
+
+        if odds_numbers.len() > 1 {
+            odds_numbers.remove(odd_index);
+        }
+
+        println!("----| Category {} | Combination n°{}", category, idx);
         println!("Numbers: {:?}, Odd number: {}\n", new_numbers, odd_number);
     }
 }
@@ -84,8 +94,8 @@ fn collect_input(input: String) -> Vec<i32> {
         .collect::<Vec<i32>>()
 }
 
-fn generate_new_numbers(input: Vec<i32>, take_nbr: usize) -> Vec<i32> {
-    let mut cloned_input = input.clone();
+fn generate_new_numbers(input: &Vec<i32>, take_nbr: usize) -> Vec<i32> {
+    let mut cloned_input = input.to_owned();
 
     cloned_input.shuffle(&mut thread_rng());
     cloned_input
